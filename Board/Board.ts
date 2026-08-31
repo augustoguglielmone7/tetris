f31f1ffe2c7ed1e9e9875182d984c6f7ab5fc05f
Mateo <dmateof31@gmail.com>
2026-08-31 09:55:56 -0300

    feat: validar límites del tablero

--- Board/Board.ts
+++ Board/Board.ts
@@ -1,11 +1,22 @@
+import { Cell } from "../interfaces/interfac";
+
 export class Board {
-    // Define el ancho y alto del tablero
+    //define el ancho y alto del tablero
     public readonly width: number;
     public readonly height: number;
 
-    // Por defecto el tablero tiene 10 columnas y 20 filas
+    // por defecto el tablero tiene 10 columnas y 20 filas
     constructor(width: number = 10, height: number = 20) {
         this.width = width;
         this.height = height;
     }
+ // comprobar si la celda está dentro de los límites del tablero.
+    public isInsideBounds(cell: Cell): boolean {
+        return cell.row >= 0 &&
+            cell.row < this.height &&
+            cell.column >= 0 &&
+            cell.column < this.width;
+    }
+
+
 }
