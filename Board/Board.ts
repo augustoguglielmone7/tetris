3dfd42d747cb8615e6d0d16fd879e9b078924e1d
Mateo <dmateof31@gmail.com>
2026-09-01 11:45:56 -0300

    feat: agregar celdas ocupadas al tablero

--- Board/Board.ts
+++ Board/Board.ts
@@ -5,18 +5,21 @@ export class Board {
     public readonly width: number;
     public readonly height: number;
 
+    // Guarda las celdas que ya están ocupadas
+    private occupiedCells: Set<string>;
+
     // por defecto el tablero tiene 10 columnas y 20 filas
     constructor(width: number = 10, height: number = 20) {
         this.width = width;
         this.height = height;
+        this.occupiedCells = new Set<string>();
     }
- // comprobar si la celda está dentro de los límites del tablero.
+
+    // comprobar si la celda está dentro de los límites del tablero.
     public isInsideBounds(cell: Cell): boolean {
         return cell.row >= 0 &&
             cell.row < this.height &&
             cell.column >= 0 &&
             cell.column < this.width;
     }
-
-
 }
