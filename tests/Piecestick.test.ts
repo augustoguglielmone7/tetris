60317f0a2de8fbeeace904ff01de1e9c7b427cb1
piragineemi <piragineemi@gmail.com>
2026-08-31 21:37:06 -0300

    piecestick test

---
+++ tests/Piecestick.test.ts
@@ -0,0 +1,21 @@
+import { Piecestick } from '../Piece/Piecestick';
+describe ('Piecestick Test', () => {
+    it('debe crear una pieza I correctamente y tener 4 elementos', () => {      
+    const piecestick = new Piecestick();
+    expect(piecestick).toBeDefined();
+    expect(piecestick).not.toBeNull();
+    });     
+
+    it('la pieza I debe poder rotar a izquierda y derecha', () => {
+        const piecestick = new Piecestick();
+        expect(typeof piecestick.rotateLeft).toBe('function');
+        expect(typeof piecestick.rotateRight).toBe('function');
+        piecestick.rotateLeft();
+        piecestick.rotateRight();
+    });
+
+});
+
+
+
+
