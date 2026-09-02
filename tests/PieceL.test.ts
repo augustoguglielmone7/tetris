da78e41dd7b4cb78a081ed2893d5973cef5603af
piragineemi <piragineemi@gmail.com>
2026-09-02 10:33:45 -0300

    piece l cambio test

--- tests/PieceL.test.ts
+++ tests/PieceL.test.ts
@@ -4,16 +4,33 @@ import { describe, test, expect } from 'vitest';
 describe('PieceL Test', () => {
     test('debe crear una pieza L correctamente y tener 4 elementos', () => {
         const pieceL = new PieceL();
-        expect(pieceL).toBeDefined();
-        expect(pieceL).not.toBeNull();
-    }
-);
-    test('la pieza L debe poder rotar a izquierda y derecha', () => {
-        const pieceL = new PieceL();
-        expect(typeof pieceL.rotateLeft).toBe('function');
-        expect(typeof pieceL.rotateRight).toBe('function');
-        pieceL.rotateLeft();
-        pieceL.rotateRight();
-    }
-);
-  });     
+
+        expect(pieceL.getCells()).toHaveLength(4);
+
+        expect(pieceL.getCells()).toEqual([
+            { row: 0, column: 2 },
+            { row: 1, column: 0 },
+            { row: 1, column: 1 },
+            { row: 1, column: 2 }
+        ]);
+    });
+
+    test('la pieza L debe rotar a la izquierda y volver a su forma inicial', () => {
+    const pieceL = new PieceL();
+
+    const initialShape = pieceL.getCells();
+
+    pieceL.rotateLeft();
+
+    expect(pieceL.getCells()).toEqual([
+        { row: 0, column: 1 },
+        { row: 1, column: 1 },
+        { row: 2, column: 1 },
+        { row: 2, column: 2 }
+    ]);
+
+    pieceL.rotateRight();
+
+    expect(pieceL.getCells()).toEqual(initialShape);
+});
+});
