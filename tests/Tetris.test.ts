b78fa1f503181fc1cfd178c047ec8462f9101522
piragineemi <piragineemi@gmail.com>
2026-09-02 01:07:10 -0300

    test mover izquierda y derecha

--- tests/Tetris.test.ts
+++ tests/Tetris.test.ts
@@ -1,5 +1,6 @@
 import { describe, test, expect } from 'vitest';
 import { Tetris } from '../Juego/tetris';
+import { PieceSquare } from '../Piece/Piecesquare';
 
 describe('Tetris Test', () => {
     test('debe instanciarse de forma correcta con un tablero', () => {
@@ -31,5 +32,25 @@ describe('Tetris Test', () => {
 
         expect(game.getBoard().getWidth()).toBe(10);
         expect(game.getBoard().getHeight()).toBe(20);
+    });
+
+    test('debe mover la pieza a la izquierda y a la derecha respetando los bordes', () => {
+        const game = new Tetris(10, 20);
+
+        (game as any).currentPiece = new PieceSquare();
+        (game as any).currentOffset = { row: 0, column: 1 };
+
+        expect(game.moveLeft()).toBe(true);
+        expect(game.getCurrentOffset()).toEqual({ row: 0, column: 0 });
+
+        (game as any).currentOffset = { row: 0, column: 0 };
+        expect(game.moveLeft()).toBe(false);
+
+        (game as any).currentOffset = { row: 0, column: 7};
+        expect(game.moveRight()).toBe(true);
+        expect(game.getCurrentOffset()).toEqual({ row: 0, column: 8});
+
+        (game as any).currentOffset = { row: 0, column: 8 };
+        expect(game.moveRight()).toBe(false);
     });
 });
