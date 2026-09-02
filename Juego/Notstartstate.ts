f407c7b2f3fde77af94e164af7bc98186508292e
guglielmonehectorsantiago <guglielmonehectorsantiago@gmail.com>
2026-09-02 14:01:53 -0300

    comi

--- Juego/Notstartstate.ts
+++ Juego/Notstartstate.ts
@@ -1,3 +1,32 @@
-export class Notstartstate {
-    public startGame(): void {
-        
+import type { Tetris } from "./Tetris";
+import type { ITetrisState, TetrisStateName } from "./interfaces/ITetrisState";
+import { RunningState } from "./RunningState";
+import { FinishedState } from "./FinishedState";
+
+export class NotStartedState implements ITetrisState {
+    public readonly name: TetrisStateName = "NotStarted";
+
+    public start(tetris: Tetris): void {
+        const spawned = tetris.trySpawnPiece();
+
+        tetris.setState(spawned ? new RunningState() : new FinishedState());
+    }
+
+    public tick(tetris: Tetris): void {}
+
+    public moveLeft(tetris: Tetris): boolean {
+        return false;
+    }
+
+    public moveRight(tetris: Tetris): boolean {
+        return false;
+    }
+
+    public rotateLeft(tetris: Tetris): boolean {
+        return false;
+    }
+
+    public rotateRight(tetris: Tetris): boolean {
+        return false;
+    }
+}
